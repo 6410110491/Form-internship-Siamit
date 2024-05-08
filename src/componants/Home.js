5d35e09f42de1d2bbde330e147bb0aa420d534eb
Poramest  Noosri <6410110307@psu.ac.th>
2024-05-08 14:40:24 +0700

    test

--- src/componants/Home.js
+++ src/componants/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Form} from 'react-bootstrap'
+import ScrollToTop from 'react-scroll-to-top'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <p style={{ fontSize: "2rem" }}>ประวัติครอบครัว</p>
+      <div style={{ height: "5px", width: "100%", backgroundColor: "#47474A" }}></div>
+
+      {/* ScroolToTop */}
+      <ScrollToTop smooth color='white' style={{ borderRadius: "20px", backgroundColor: "#F3C710" }} />
+    </div>
+  
   )
 }
 
